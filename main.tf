d66e49ce6c106a39ae7adfbbd322caedc0fb26f6
josesebn <josesebn@gmail.com>
2025-01-26 20:04:04 -0500

    adding private and public subnets

--- main.tf
+++ main.tf
@@ -3,3 +3,42 @@ resource "aws_vpc" "ansystest_vpc" {
   enable_dns_support   = true
   enable_dns_hostnames = true
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.ansystest_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.ansystest_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "public_subnet_3" {
+  vpc_id                  = aws_vpc.ansystest_vpc.id
+  cidr_block              = "10.0.3.0/24"
+  availability_zone       = "us-east-1c"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.ansystest_vpc.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.ansystest_vpc.id
+  cidr_block        = "10.0.5.0/24"
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_subnet" "private_subnet_3" {
+  vpc_id            = aws_vpc.ansystest_vpc.id
+  cidr_block        = "10.0.6.0/24"
+  availability_zone = "us-east-1c"
+}
