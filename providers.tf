1d03ab2ad847594ac512b998e56afe7c29fd26a8
josesebn <josesebn@gmail.com>
2025-01-26 20:53:55 -0500

    adding terraform local and aws provider

--- providers.tf
+++ providers.tf
@@ -1,5 +1,13 @@
-provider "aws" {
-  region = "us-east-1" 
-#   access_key = "your-access-key"
-#   secret_key = "your-secret-key"
+terraform {
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.2"
+    }
+    aws = {
+        region = "us-east-1" 
+    #   access_key = "your-access-key"
+    #   secret_key = "your-secret-key"
+    }
+  }
 }
