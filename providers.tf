58359a885085353d3cd4fa40d78dcf3e028d1b5c
josesebn <josesebn@gmail.com>
2025-01-26 20:57:15 -0500

    fix provider formatting 1

--- providers.tf
+++ providers.tf
@@ -5,9 +5,13 @@ terraform {
       version = "~> 2.2"
     }
     aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+}
+provider "aws" {
         region = "us-east-1" 
     #   access_key = "your-access-key"
     #   secret_key = "your-secret-key"
     }
-  }
-}
