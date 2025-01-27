e2305c67d7fdb366e6705873b94e37acd1de4dca
josesebn <josesebn@gmail.com>
2025-01-26 20:13:58 -0500

    adding public subnets access to internet

--- main.tf
+++ main.tf
@@ -4,6 +4,34 @@ resource "aws_vpc" "ansystest_vpc" {
   enable_dns_hostnames = true
 }
 
+resource "aws_internet_gateway" "ansysigw" {
+  vpc_id = aws_vpc.ansystest_vpc.id
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.ansystest_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.ansysigw.id
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "public_subnet_3_association" {
+  subnet_id      = aws_subnet.public_subnet_3.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+
 resource "aws_subnet" "public_subnet_1" {
   vpc_id                  = aws_vpc.ansystest_vpc.id
   cidr_block              = "10.0.1.0/24"
